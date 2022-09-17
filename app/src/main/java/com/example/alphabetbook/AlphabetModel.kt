95c49d9c6e56c030de4b6ef4f0c8bdc510918cc9
Maqhobosheane Mohlerepe <maqhobosheane@outlook.com>
2022-09-17 14:45:25 +0200

    Add thread to load image in background

--- app/src/main/java/com/example/alphabetbook/AlphabetModel.kt
+++ app/src/main/java/com/example/alphabetbook/AlphabetModel.kt
@@ -1,6 +1,23 @@
 package com.example.alphabetbook
 
-class AlphabetModel(imageName: String) {
-    private var image = imageName
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.ImageView
 
+class AlphabetModel(_image: String, _imageView: ImageView, _resources: Resources, _packageName: String): Thread() {
+    private var imageName = _image
+    private var imageView = _imageView
+    private var resources = _resources
+    private var packageName = _packageName
+
+    override fun run() {
+        var id = resources.getIdentifier(imageName, "drawable", packageName)
+        var bitmap = BitmapFactory.decodeResource(resources, id)
+        imageView.setImageBitmap(bitmap)
+    }
+
+    fun loadImage() {
+        start()
+    }
 }
