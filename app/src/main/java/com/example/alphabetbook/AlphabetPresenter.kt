26d83622c2499a505408071b3d246649e1003a8e
Maqhobosheane Mohlerepe <maqhobosheane@outlook.com>
2022-09-17 12:59:28 +0200

    Add function to display toast

--- app/src/main/java/com/example/alphabetbook/AlphabetPresenter.kt
+++ app/src/main/java/com/example/alphabetbook/AlphabetPresenter.kt
@@ -1,4 +1,22 @@
 package com.example.alphabetbook
 
-class AlphabetPresenter {
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlphabetPresenter (_intent: Intent, _context: Context) {
+    // Create intent and context properties
+    private var intent = _intent
+    private var context = _context
+
+    // Get the alphabet name from intent
+    var alphabet = intent.getStringExtra("letter") ?: ""
+
+    // displays Toast: The alphabet e.g., A
+    fun displayToast() {
+        Toast.makeText(context, alphabet, Toast.LENGTH_LONG).show()
+    }
+
+    // Load image
+
 }
