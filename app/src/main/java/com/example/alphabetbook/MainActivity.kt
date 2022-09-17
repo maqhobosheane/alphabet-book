83a6443d604917362f1b368bf371f9a8ad9663ca
Maqhobosheane Mohlerepe <maqhobosheane@outlook.com>
2022-09-17 21:04:32 +0200

    Add final main activity

--- app/src/main/java/com/example/alphabetbook/MainActivity.kt
+++ app/src/main/java/com/example/alphabetbook/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.alphabetbook
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
