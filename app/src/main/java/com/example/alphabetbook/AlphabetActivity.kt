68cd8876467e772b3c506d63134683f6bd59a27f
Maqhobosheane Mohlerepe <maqhobosheane@outlook.com>
2022-09-17 14:46:45 +0200

    Load image using the presenter

--- app/src/main/java/com/example/alphabetbook/AlphabetActivity.kt
+++ app/src/main/java/com/example/alphabetbook/AlphabetActivity.kt
@@ -2,6 +2,7 @@ package com.example.alphabetbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class AlphabetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,10 @@ class AlphabetActivity : AppCompatActivity() {
 
         val presenter = AlphabetPresenter(intent, applicationContext)
         presenter.displayToast()
+
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        presenter.loadImage(imageView, resources, packageName)
+
+
     }
 }
