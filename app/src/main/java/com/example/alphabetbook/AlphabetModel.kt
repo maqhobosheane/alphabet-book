2f54978ffa5dd793e899f6ae3ea36986de24e7d5
Maqhobosheane Mohlerepe <maqhobosheane@outlook.com>
2022-09-17 15:41:18 +0200

    Add function to load image

--- app/src/main/java/com/example/alphabetbook/AlphabetModel.kt
+++ app/src/main/java/com/example/alphabetbook/AlphabetModel.kt
@@ -4,26 +4,17 @@ import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.widget.ImageView
+import java.util.concurrent.Executors
 
-class LoadInBackground(_image: String, _imageView: ImageView, _resources: Resources, _packageName: String): Thread() {
-    private var imageName = _image
-    private var imageView = _imageView
-    private var resources = _resources
-    private var packageName = _packageName
-
-    override fun run() {
-        var id = resources.getIdentifier(imageName, "drawable", packageName)
-        var bitmap = BitmapFactory.decodeResource(resources, id)
-        imageView.setImageBitmap(bitmap)
-    }
-}
-class AlphabetModel(_image: String, _imageView: ImageView, _resources: Resources, _packageName: String): Thread() {
+class AlphabetModel(_image: String, _imageView: ImageView, _resources: Resources, _packageName: String) {
     private var imageName = _image
     private var imageView = _imageView
     private var resources = _resources
     private var packageName = _packageName
 
     fun loadImage() {
-        LoadInBackground(imageName, imageView, resources, packageName).start()
+        var id = resources.getIdentifier(imageName, "drawable", packageName)
+        var bitmap = BitmapFactory.decodeResource(resources, id)
+        imageView.setImageBitmap(bitmap)
     }
 }
