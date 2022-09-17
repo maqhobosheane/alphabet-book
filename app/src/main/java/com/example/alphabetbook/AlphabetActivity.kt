e0e748ee53235ad756dbd5c1e9c139c0b2ca5192
Maqhobosheane Mohlerepe <maqhobosheane@outlook.com>
2022-09-17 17:36:38 +0200

    Handle overview action

--- app/src/main/java/com/example/alphabetbook/AlphabetActivity.kt
+++ app/src/main/java/com/example/alphabetbook/AlphabetActivity.kt
@@ -3,6 +3,8 @@ package com.example.alphabetbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 
@@ -37,5 +39,20 @@ class AlphabetActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.buttons_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_overview) {
+            var intent = Intent(applicationContext, MainActivity::class.java)
+            intent.putExtra("name", "Overview")
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
